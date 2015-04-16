6260ba1a7d0a1c0b2ae83cc758d9c32536e1b9f1
ruddernation <ruddernation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 20:01:59 +0000

    First Advanced chat version - hope you like.
    git-svn-id: https://plugins.svn.wordpress.org/rnd-video-chat/trunk@1136538 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,127 @@
+=== Wordpress Video Chat Advanced ===
+
+
+Contributors: ruddernation
+
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FFAC7FBEBH6JE
+
+Tags: video chat, tinychat, chat, wordpress chat, buddypress chat, wordpress video chat, buddypress video chat
+
+Requires at least: 3.6.0
+
+Tested up to: 4.1.1
+
+Stable tag: 1.0.0
+
+License: GPLv3
+
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+
+== Description ==
+
+
+TinyChat full screen video chat for WordPress/BuddyPress, 
+This also has YouTube/SoundCloud for all chatters and now has smileys enabled using my embed file, 
+This advanced version allows you to add your own room name and allows you to input your own personal data like autoop and prohash info as well as account name, To get the autoop and prohash codes then you need to make sure you're logged in on tinychats domain, I've added screenshots of how to get the hash codes to use, Also if you're not logged in the on account name you can use any name you want even if it is already registered.
+
+
+== Installation ==
+
+
+This will automatically create the page and install the short code with link *domain name/chatroom-advanced*, If it does not then please read the rest below.
+
+Simply use shortcode [rnd_tinychat_page] in a page.
+
+For this you'll need to get your own autoop/prohash codes, These are found when you view page source on tinychat.com/selectedroom ,
+Around line 449, I've provided screenshots for you to view,
+
+This also uses my modified Tinychat embed file, This get's updated externally so you'll never need to update it,
+
+If you want to use Tinychats original then you'll have to change the embed file url to http://tinychat.com/embed/chat.js.
+
+Some themes may have issues with keyboard shortcuts, Especially for those that use jetpack, You'll have to disable the admin navbar from your frontend by adding 
+
+// Remove Admin Bar Front End
+
+add_filter('show_admin_bar', '__return_false');
+
+to your themes functions.php file.
+
+
+== Screenshots ==
+
+* Right click to view page source or go to browsers menu and select it from there.
+
+* This shows you the autoop code, This is only visible if you're a admin or a moderator of a room, Prohash will also show on that line but as a larger number.
+
+* These are examples of the hash codes you'll need to enter for your selected room.
+
+* TinyChat load screen, Users of TinyChat will notice the colour change.
+
+* Once loaded you'll have to enter a name.
+
+* This is showing the YouTube/Soundcloud buttons.
+
+* Both show popups which you just enter what you'd like to search.
+
+* This is how it displays with both YouTube and Soundcloud.
+
+* Click this to maximize the video size, This also works when you're on camera.
+
+* This is how it looks using the larger video's.
+
+
+== Notes ==
+
+
+This is full screen video chat that is used on TinyChat,
+This will allow you to input your own room name unlike my other version which has a set room name,
+You can also add your autoop and prohash codes in to this when you're logged in to tinychat to show your pro icon and star,
+If anyone else requires another feature or has an idea for me to implement, Then open a support ticket with the relevant information.
+
+
+== Frequently Asked Questions ==
+
+
+* Q. Can I use this if I'm not logged in?
+
+* A. Yes - if you want this disabled then I've provided the code in the file for you to edit yourself.
+
+
+* Q. The chat is not loading for me.
+
+* A. Check to see if you have the Adobe flash player installed (http://helpx.adobe.com/flash-player.html) and JavaScript enabled in your browser.
+
+
+* Q. How do I add it to my blog/website?
+
+* A. Just go to the backend and on appearance select menus, From there you can add your page, It'll be *chatroom* by default.
+
+
+* Q. I'm having issues with my wordpress keyboard shortcuts affecting my chat, It's not allowing me to use certain letters.
+
+* A. The fix for this is to disable the Admin navbar on your frontend only, to do this add * // Remove Admin Bar Front End
+
+add_filter('show_admin_bar', '__return_false'); * to you funtions.php file in your themes folder.
+
+
+== Changelog ==
+
+
+= 1.0.1 =
+
+* First version.
+
+
+== Social Sites ==
+
+* Facebook - https://www.facebook.com/rndtc
+
+* Twitter - https://twitter.com/R_N_Designs
+
+* Github - https://github.com/ruddernation
+
+* WordPress - https://profiles.wordpress.org/ruddernation
+
+* Skype - ruddernation.designs
---
+++ rnd-tinychat.php
@@ -0,0 +1,113 @@
+<?php
+/*
+* Plugin Name: Wordpress Video Chat Advanced
+* Plugin URI: https://www.ruddernation.com
+* Author: Ruddernation Designs
+* Author URI: http://profiles.wordpress.org/ruddernation
+* Description: TinyChat full screen video chat for WordPress/BuddyPress, This also has YouTube/SoundCloud for all chatters and now has smileys enabled using my embed file, This advanced version allows you to add your own room name and allows you to input your own personal data like autoop and prohash info as well as account name.
+* Requires at least: WordPress 4.0, BuddyPress 1.8.1
+* Tested up to: WordPress 4.1.1, BuddyPress 2.2.2.1
+* Version: 1.0.0
+* License: GPLv3
+* License URI: http://www.gnu.org/licenses/gpl-3.0.html
+* Date: 16th April 2015
+*/
+define('COMPARE_VERSION', '1.0.0');
+register_activation_hook(__FILE__, 'wordpress_chat_advanced_install');
+function wordpress_chat_advanced_install() {
+	
+	global $wpdb, $wp_version;
+
+	$post_date = date("Y-m-d H:i:s");
+
+	$post_date_gmt = gmdate("Y-m-d H:i:s");
+
+	$sql = "SELECT * FROM ".$wpdb->posts." WHERE post_content LIKE '%[rnd_tinychat_page]%' AND `post_type` NOT IN('revision') LIMIT 1";
+
+	$page = $wpdb->get_row($sql, ARRAY_A);
+
+	if($page == NULL) {
+
+		$sql ="INSERT INTO ".$wpdb->posts."(
+
+			post_author, post_date, post_date_gmt, post_content, post_content_filtered, post_title, post_excerpt,  post_status, comment_status, ping_status, post_password, post_name, to_ping, pinged, post_modified, post_modified_gmt, post_parent, menu_order, post_type)
+
+			VALUES
+
+			('1', '$post_date', '$post_date_gmt', '[rnd_tinychat_page]', '', 'chatroom-advanced', '', 'publish', 'closed', 'closed', '', 'chatroom-advanced', '', '', '$post_date', '$post_date_gmt', '0', '0', 'page')";
+
+
+
+		$wpdb->query($sql);
+
+		$post_id = $wpdb->insert_id;
+
+		$wpdb->query("UPDATE $wpdb->posts SET guid = '" . get_permalink($post_id) . "' WHERE ID = '$post_id'");
+
+	} else {
+
+		$post_id = $page['ID'];
+
+	}
+
+	update_option('wordpress_chat_advanced_url', get_permalink($post_id));
+
+}
+
+add_filter('the_content', 'wp_show_wordpress_chat_advanced_page', 67);
+
+
+
+function wp_show_wordpress_chat_advanced_page($content = '') {
+
+
+
+	if(preg_match("/\[rnd_tinychat_page\]/",$content)) {
+
+		wp_show_wordpress_chat_advanced();
+
+
+		return "";
+
+	}
+
+	return $content;
+
+}
+
+function wp_show_wordpress_chat_advanced() {
+
+	if(!get_option('wordpress_chat_advanced_enabled', 0)) {
+
+	}
+
+	?>
+<?php $room = $_POST ['room']; $username = $_POST ['username']; $autoop = $_POST ['autoop']; $prohash = $_POST ['prohash'];$account = $_POST ['account']; $url = ''; $current_user = wp_get_current_user(); 
+echo ' 
+<form action="'.$url.'" method="post" class="form">Room:<input type="text" name="room" title="Enter Room Name, If it does not exist then it will create the room for you." placeholder="Example:ruddernation"/>
+Autoop:<input type="text" name="autoop" id="autoop" title="Enter your autoop code, This is needed for admins of the room only."/>
+Prohash:<input type="text" name="prohash" id="prohash" title="Enter prohash code, If your a pro user only."/> 
+Account:<input type="text" name="account" id="account" title="If not logged in on Tinychat then whatever account name you put here you will clone if it exists." placeholder="Example: ruddernation"/>
+<input type="submit" class="button" value="Enter"/></form></div>'; ?>
+
+		<style>#chat{position:fixed;left:0px;right:0px;bottom:0px;height:98%;width:100%;z-index:9999}</style>
+<?php 
+if((preg_match("/^[a-zA-Z0-9]{3,}/", $_POST['room']) == '1'))
+{
+if($room !== 'room')
+$room=(strlen($room) > 32) ? substr($room,0,32).'' : $room;
+$autoop=(strlen($autoop) > 64) ? substr($autoop,0,64).'' : $autoop;
+$prohash=(strlen($prohash) > 64) ? substr($prohash,0,64).'' : $prohash;
+$account=(strlen($account) > 32) ? substr($account,0,32).'' : $account;
+$room=htmlspecialchars($room,ENT_QUOTES, 'UTF-8');
+$autoop=htmlspecialchars($autoop,ENT_QUOTES, 'UTF-8');
+$account=htmlspecialchars($account,ENT_QUOTES, 'UTF-8');
+$prohash=htmlspecialchars($prohash,ENT_QUOTES, 'UTF-8');
+$room=preg_replace('/[^a-z0-9]/s', '', $room);
+$account=preg_replace('/[^a-zA-Z0-9]/s', '', $account);
+$prohash=preg_replace('/[^0-9]/s', '', $prohash);
+$autoop=preg_replace('/[^0-9]/s', '', $autoop);
+{		
+echo '<div id="chat"><script   data-cfasync="false" src="https://www.ruddernation.info/info/js/slagmodified.js"></script><script   data-cfasync="false" type=text/javascript>
+var embed;
+embed = tinychat({room: "'.$room.'",';{echo' autoop: "'.$autoop.'",';}{echo' account: "'.$account.'",';}{echo' prohash: "'.$prohash.'",';} echo 'nick: "' . $current_user->display_name .'"'; echo '})</script><div id="Ruddernation"></div></div>';}}}?>
